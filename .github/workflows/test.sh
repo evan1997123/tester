2462beae0b9eb3d1e658a6c124d0cb951d754335
Evan Sum <evansum@evans-macbook.lan>
2021-02-13 15:05:08 -0800

    updated bash script for appending on evanlovescodio

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -3,4 +3,14 @@ if [[ `git branch -a 2>&1` = *evanlovescodio* ]]; then
     echo "OK"
 else
     echo "Sad reacts"
+    git branch evanlovescodio
 fi
+
+echo "before checkout"
+git checkout evanlovescodio
+echo "Goodbye, world!\n" >> ./a.txt
+echo "after appending"
+cat ./a.txt
+git add .
+git commit -m "Added goodbye world to a.txt in branch evanlovescodio"
+# git push origin evanlovescodio
